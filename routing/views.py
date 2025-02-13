8ecdfcc948432258ae56b8a3203314ce95bd3067
Suhag <suhagsundarswain1@gmail.com>
2025-02-13 23:13:03 +0530

    v1.1.0-Navigation coordinates added.

--- routing/views.py
+++ routing/views.py
@@ -9,30 +9,39 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+API_KEY = getenv("API_KEY")
+
 def get_lat_lon(location):
     url = "https://api.geoapify.com/v1/geocode/search"
     params = {
         "text":location,
-        "apiKey": getenv("API_KEY")
+        "apiKey": API_KEY
     }
-    try:
-        response = requests.get(url,params=params)
-        if not response.ok:
-            raise Exception(f"Somthing went wrong in getting {location} location details")
-        resp = response.json()
-        features = resp.get("features",[])
-        if not features:
-            raise Exception(f"Not getting {location} features.")
-        properties = features[0].get("properties",{})
-        latitude = properties.get("lat")
-        longitude = properties.get("lon")
-        city = properties.get("city")
-        return {"location":city,"latitude":latitude,"longitude":longitude}
-    except Exception as e:
-        raise e
+    response = requests.get(url,params=params)
+    if not response.ok:raise Exception(f"Somthing went wrong in getting {location} location details")
+    resp = response.json()
+    features = resp.get("features",[])
+    if not features:raise Exception(f"Not getting {location} features.")
+    properties = features[0].get("properties",{})
+    latitude = properties.get("lat")
+    longitude = properties.get("lon")
+    city = properties.get("city")
+    return {"location":city,"latitude":latitude,"longitude":longitude}
     
-def get_route():
-    return
+def get_route(start,end):
+    url="https://api.geoapify.com/v1/routing"
+    params ={
+        "waypoints":f"{start['latitude']},{start['longitude']}|{end['latitude']},{end['longitude']}",
+        "mode":"truck",
+        "units":"imperial",
+        "apiKey": API_KEY
+    }
+    response = requests.get(url,params=params)
+    if not response.ok:raise Exception("Not getting route. Try again after some time")
+    resp = response.json()
+    features = resp.get("features",[])
+    if not features:raise Exception("Not getting route.")
+    return features[0]
 
 def get_route_with_fuel_stopage(request):
     try:
@@ -45,8 +54,13 @@ def get_route_with_fuel_stopage(request):
         
         start_location_details = get_lat_lon(start_location)
         end_location_details = get_lat_lon(end_location)
+        route_data = get_route(start_location_details,end_location_details)
+        route_coords = route_data.get("geometry").get("coordinates")[0]
+        
 
-        return JsonResponse({"start":start_location_details,"end":end_location_details})
+        
+
+        return JsonResponse({"route_data":route_coords})
     
     except KeyError as e:
         return JsonResponse({"error":str(e)},status=400)
